36d049afa348f3d33099ab1596ad65a92645bcd6
Daniel Sadavoy <daniel.sadavoy@gmail.com>
2023-05-19 11:01:56 -0400

    flask top, react in folder

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS #comment this on deployment
+from api.HelloApiHandler import HelloApiHandler
+
+app = Flask(__name__, static_url_path='', static_folder='front/')
+CORS(app) #comment this on deployment
+api = Api(app)
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+@app.route("/helloworld")
+def helloworld():
+    return "Hello world!"
+    
+api.add_resource(HelloApiHandler, '/flask/hello')
